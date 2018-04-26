cd5406c1abc78b504e5ea5b1c6253429836152a1
Khyojung <918fall@gamil.com>
2018-04-26 13:51:13 +0900

    add remark of neseccary function, add stream of blackjack

--- src/main/java/com/cnu/blackjack/Dealer.java
+++ src/main/java/com/cnu/blackjack/Dealer.java
@@ -4,6 +4,9 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class Dealer {
 
+    //덱을 가질 수 있는 생성자 만들어주기
+    //evaluator에서 사용한다.
+
     public int getDealerScore() {
         ThreadLocalRandom random = ThreadLocalRandom.current();
         int score = random.nextInt(17, 25);
--- src/main/java/com/cnu/blackjack/Evaluator.java
+++ src/main/java/com/cnu/blackjack/Evaluator.java
@@ -14,9 +14,12 @@ public class Evaluator {
     }
 
     public void start() {
-
+        //Game객체를 생성해준다. -> 생성자에 새로운 덱을 생성해서 넘겨줌 Game game = new Game(new Deck(number));
+        //Game.start를 호출해준다.
     }
 
+    //hitCard() = 덱에서 카드 한장을 뽑아서 사용자의 카드 리스트에 추가해준다.
+    //맨 처음에만 호출하는거
     private void dealCardToPlayers() {
         playerMap.forEach((name, player) -> {
             player.hitCard();
